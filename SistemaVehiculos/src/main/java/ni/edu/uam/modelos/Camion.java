e0b3e3725b949994127ae3fdda66615e1f4427a6
FICT1 <isaquitopro10@gmail.com>
2026-05-04 18:31:56 -0600

    Clase Motocicleta

--- SistemaVehiculos/src/main/java/ni/edu/uam/modelos/Camion.java
+++ SistemaVehiculos/src/main/java/ni/edu/uam/modelos/Camion.java
@@ -1,4 +1,19 @@
 package ni.edu.uam.modelos;
 
-public class Camion {
+public class Camion extends Vehiculo{
+    private double capacidadCarga;
+    private int numeroejes;
+
+
+    public Camion() {
+    }
+
+    public Camion(String marca, String modelo, int anio, double velocidadMaxima,
+                  double capacidadCarga, int numeroejes) {
+        super(marca, modelo, anio, velocidadMaxima);
+        this.capacidadCarga = capacidadCarga;
+        this.numeroejes = numeroejes;
+    }
+
+    
 }
--- SistemaVehiculos/src/main/java/ni/edu/uam/modelos/Motocicleta.java
+++ SistemaVehiculos/src/main/java/ni/edu/uam/modelos/Motocicleta.java
@@ -1,4 +1,42 @@
 package ni.edu.uam.modelos;
 
-public class Motocicleta {
+public class Motocicleta extends Vehiculo{
+    private int cilindrada;
+    private String tipo;
+
+
+    public Motocicleta(){
+    }
+
+    public Motocicleta(String marca, String modelo, int anio, double velocidadMaxima,
+                       int cilindrada, String tipo) {
+        super(marca, modelo, anio, velocidadMaxima);
+        this.cilindrada = cilindrada;
+        this.tipo = tipo;
+    }
+
+    public StringBuilder mostrarInformacion() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("=== MOTOCICLETA ===\n");
+        sb.append(super.mostrarInformacion());
+        sb.append("Cilindrada: ").append(cilindrada).append(" cc\n");
+        sb.append("Tipo: ").append(tipo).append("\n");
+        return sb;
+    }
+
+    public int getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(int cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
