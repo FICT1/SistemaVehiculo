4d3d7745b1e2f136b400f08314e071938f80fd02
FICT1 <isaquitopro10@gmail.com>
2026-05-04 18:34:02 -0600

    Clase Camion

--- SistemaVehiculos/src/main/java/ni/edu/uam/modelos/Camion.java
+++ SistemaVehiculos/src/main/java/ni/edu/uam/modelos/Camion.java
@@ -15,5 +15,28 @@ public class Camion extends Vehiculo{
         this.numeroejes = numeroejes;
     }
 
-    
+    public StringBuilder mostrarInformacion() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("=== CAMION ===\n");
+        sb.append(super.mostrarInformacion());
+        sb.append("Capacidad de Carga: ").append(capacidadCarga).append(" toneladas\n");
+        sb.append("Número de Ejes: ").append(numeroejes).append("\n");
+        return sb;
+    }
+
+    public double getCapacidadCarga() {
+        return capacidadCarga;
+    }
+
+    public void setCapacidadCarga(double capacidadCarga) {
+        this.capacidadCarga = capacidadCarga;
+    }
+
+    public int getNumeroejes() {
+        return numeroejes;
+    }
+
+    public void setNumeroejes(int numeroejes) {
+        this.numeroejes = numeroejes;
+    }
 }
